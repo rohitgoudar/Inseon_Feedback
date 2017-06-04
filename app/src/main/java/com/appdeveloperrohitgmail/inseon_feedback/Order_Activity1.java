30cea51ee90f74a582d824710d3a291c671ca6af
Rohit Goudar <rohitgoudar444@gmail.com>
2017-06-04 19:21:22 +0530

    suevey started

--- app/src/main/java/com/appdeveloperrohitgmail/inseon_feedback/Order_Activity1.java
+++ app/src/main/java/com/appdeveloperrohitgmail/inseon_feedback/Order_Activity1.java
@@ -1,13 +1,25 @@
 package com.appdeveloperrohitgmail.inseon_feedback;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Order_Activity1 extends AppCompatActivity {
 
+    Button order_next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_1);
+        order_next = (Button)findViewById(R.id.order_next);
+        order_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent orderIntent = new Intent(Order_Activity1.this,Survey_Activity1.class);
+                startActivity(orderIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/appdeveloperrohitgmail/inseon_feedback/StartActivity.java
+++ app/src/main/java/com/appdeveloperrohitgmail/inseon_feedback/StartActivity.java
@@ -1,13 +1,25 @@
 package com.appdeveloperrohitgmail.inseon_feedback;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class StartActivity extends AppCompatActivity {
 
+    ImageButton start_survey;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        start_survey = (ImageButton)findViewById((R.id.startButton));
+        start_survey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(StartActivity.this,Order_Activity1.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
